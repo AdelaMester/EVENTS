4020770d0a28b37a1f2b7406014f0cfc2768ad3d
AdelaMester <adelamester@yahoo.co.uk>
2021-05-01 16:01:24 +0000

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn NovaFutur.application:app
---
+++ test_hello.py
@@ -0,0 +1,19 @@
+from application import app
+'''
+def test_index(self):
+    assert True
+
+def test_index_2(client):
+    response=client.get("/", content_type="html/text")
+    assert response.status_code == 200
+'''
+
+def test_index():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b"Welcome to our Event Ticket Management page! "
+'''
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client'''
